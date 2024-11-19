64f0db872dd197354ed2b90fc63930cdbf103e20
sherlynkek <sherlynkek00@gmail.com>
2024-11-19 14:25:31 +0800

    main

---
+++ src/main/java/sg/edu/nus/iss/vttp5_day1w/Vttp5Day1wApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_day1w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5Day1wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5Day1wApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp5_day1w/Vttp5Day1wApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_day1w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5Day1wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
